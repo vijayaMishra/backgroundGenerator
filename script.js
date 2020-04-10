0474ad3140818e5de37b7bd0b3b50dc53a999a51
vijayaMishra <37749116+vijayaMishra@users.noreply.github.com>
2020-04-10 23:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+	css.textContent = body.style.background + ";";
+
 }
 color1.addEventListener("input", setGradient);
 
