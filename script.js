a6664eed3a8b5d69ed647024970dc53bcd1154d6
vijayaMishra <37749116+vijayaMishra@users.noreply.github.com>
2020-04-10 23:48:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
 //body.style.background = "pink";
 
-color1.addEventListener("input", function() {
+function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-})
+}
+color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", function() {
-	//console.log(color2.value);
-	body.style.background = "linear-gradient(to left, " + color2.value + ", " + color1.value + ")";
-})
+color2.addEventListener("input", setGradient);
+
+
+// color1.addEventListener("input", function() {
+// 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+// })
+
+// color2.addEventListener("input", function() {
+// 	//console.log(color2.value);
+// 	body.style.background = "linear-gradient(to left, " + color2.value + ", " + color1.value + ")";
+// })
